3baa60f233e9fecdeee9b600a4008d3ce0edad69
Thebigbot <77632836+Thebigbot0000@users.noreply.github.com>
2022-03-25 19:01:14 +0100

    food(pizza): ready

--- index.ts
+++ index.ts
@@ -1,5 +1,11 @@
-(async () => {
-  await console.log('Sussy giulio was here');
-})
+import * as _dds_ from "discord.js";
+import * as _j from "./main.json";
+import * as _dev from "./packages/dev";
 
+const _ = new _dds_.Client({ intents: [_dds_.Intents.FLAGS.GUILDS, _dds_.Intents.FLAGS.GUILD_MESSAGES] });
+
+
+_.login(_j.token)
+
+_.on('ready', () => _dev.run())
 
