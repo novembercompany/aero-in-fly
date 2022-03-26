ad9fc0d633afa91e08a9262cd88c10325ace03ca
Thebigbot <77632836+Thebigbot0000@users.noreply.github.com>
2022-03-26 07:55:47 +0100

    food(pasta): @aero/dev

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import * as _dds_ from "discord.js";
 import * as _j from "./main.json";
-import * as _dev from "./packages/dev";
+import * as _dev from "@aero/dev";
 
 const _ = new _dds_.Client({ intents: [_dds_.Intents.FLAGS.GUILDS, _dds_.Intents.FLAGS.GUILD_MESSAGES] });
 
@@ -8,4 +8,12 @@ const _ = new _dds_.Client({ intents: [_dds_.Intents.FLAGS.GUILDS, _dds_.Intents
 _.login(_j.token)
 
 _.on('ready', () => _dev.run())
+
+_.on('messageCreate', (msg) => {
+  if (msg.content === '!play') _dev.play(msg, _);
+  if (msg.content === '!help') _dev.help(msg, _);
+  if (msg.content === '!ping') _dev.ping(msg, _);
+  if (msg.content === '!loop') _dev.loop(msg, _);
+  if (msg.content === '!stop') _dev.stop(msg, _);
+})
 
--- packages/dev/index.ts
+++ packages/dev/index.ts
@@ -1,3 +1,26 @@
-export const run = () => {
-    console.log("@_@ Realy?")
+import { MessageEmbed, Message, Client } from "discord.js";
+
+interface Dev {
+    run: () => void;
+    play: () => void;
+    stop: () => void;
+    loop: () => void;
+    help: () => void;
+    ping: () => void;
+}
+
+const run = () => {};
+
+const play = (msg: Message, client: Client) => {};
+
+const stop = (msg: Message, client: Client) => {};
+
+const loop = (msg: Message, client: Client) => {};
+
+const help = (msg: Message, client: Client) => {};
+
+const ping = (msg: Message, client: Client) => {
+    msg.channel.send("🏓 **Pong**, `"+client.ws.ping+"ms`")
 };
+
+export { run, ping, stop, loop, help, play, Dev }
